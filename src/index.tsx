701a296a17ee1e551b330bd030383577d3da7688
Emil Arzumanov <22-emil@mail.ru>
2023-10-06 18:27:03 +0500

    Basic architecture

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+
+const rootElement: HTMLElement | null = document.getElementById('root');
+if (rootElement === null) {
+	throw new Error('Failed to find root element');
+}
+
+ReactDOM.createRoot(rootElement).render(
+	<App />,
+);
